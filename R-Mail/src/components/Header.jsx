e7abdb164cf5334dcf5186882b4d5dfdbeace079
NuBreed <ayodejiadio@gmail.com>
2023-08-03 20:16:30 +0200

    header-2

--- R-Mail/src/components/Header.jsx
+++ R-Mail/src/components/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <>
+      <header
+        style={{
+          backgroundColor: '#001133',
+          width: '100vw',
+          height: '2rem',
+          color: '#ffffff',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div>logo</div>
+        <div> navMeun</div>
+      </header>
+    </>
+  )
 }
 
 export default Header
